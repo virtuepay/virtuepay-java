880381587e10a917cf42fc7db9079d19535b53a1
fioman <fioman@qq.com>
2022-03-31 02:48:33 +0800

    typo

--- example/src/main/java/cn/virtuepay/example/Main.java
+++ example/src/main/java/cn/virtuepay/example/Main.java
@@ -7,14 +7,15 @@ import java.security.SecureRandom;
 import java.util.Properties;
 
 /**
+ *
  */
 public class Main {
     /**
-   * 设置请求签名密钥，密钥对需要你自己用 openssl 工具生成，如何生成可以参考帮助中心：https://help.virtuepay.cn/article/123161；
-   * 生成密钥后，需要在代码中设置请求签名的私钥(rsa_private_key.pem)；
-   * 然后登录 [Dashboard](https://dashboard.virtuepay.cn)->点击右上角公司名称->开发信息->商户公钥（用于商户身份验证）
-   * 将你的公钥复制粘贴进去并且保存->先启用 Test 模式进行测试->测试通过后启用 Live 模式
-   */
+     * 设置请求签名密钥，密钥对需要你自己用 openssl 工具生成，如何生成可以参考帮助中心：https://help.virtuepay.cn/article/123161；
+     * 生成密钥后，需要在代码中设置请求签名的私钥(rsa_private_key.pem)；
+     * 然后登录 [Dashboard](https://dashboard.virtuepay.cn)->点击右上角公司名称->开发信息->商户公钥（用于商户身份验证）
+     * 将你的公钥复制粘贴进去并且保存->先启用 Test 模式进行测试->测试通过后启用 Live 模式
+     */
 
     // 你生成的私钥路径
     private final static String privateKeyFilePath = "res/your_rsa_private_key_pkcs8.pem";
@@ -48,6 +49,6 @@ public class Main {
     }
 
     public static int currentTimeSeconds() {
-        return (int)(System.currentTimeMillis() / 1000);
+        return (int) (System.currentTimeMillis() / 1000);
     }
 }
